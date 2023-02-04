59d1b81241711f19c4865f893ab11f993cfd233c
Irfannur Diah <kang.fand@gmail.com>
2023-02-04 19:22:45 +0700

    Update setup.sh

--- installer/setup.sh
+++ installer/setup.sh
@@ -1,5 +1,16 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+echo ""----------------------------------------------------------------------------- ""
+echo "Termux Installer"
+echo "---------------------------------------------------------------------------------"
+
+echo
+echo "."
+
+apt update -y                   
+
 # Git
-pkg install git
+pkg install git -y
 git config --global credential.helper store
 git config --global user.email "..."
 git config --global user.name "..."
@@ -8,3 +19,37 @@ git config --global user.name "..."
 pkg install vim
 pkg install nvim
 pkg install htop
+
+
+   
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+
+echo " Telah berhasil di install "
+
+exit
+
