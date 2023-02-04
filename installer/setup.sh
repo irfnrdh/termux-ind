7ebc38d299ada53f0c0d478372d294c28438654f
Irfannur Diah <kang.fand@gmail.com>
2023-02-04 11:29:02 +0700

    Draft

---
+++ installer/setup.sh
@@ -0,0 +1,10 @@
+# Git
+pkg install git
+git config --global credential.helper store
+git config --global user.email "..."
+git config --global user.name "..."
+
+# Essentials
+pkg install vim
+pkg install nvim
+pkg install htop
