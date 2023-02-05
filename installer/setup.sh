17be37bcb68db3feda6f30bce2f0e3a8d863046a
Irfannur Diah <kang.fand@gmail.com>
2023-02-06 02:37:16 +0700

    Update

--- installer/setup.sh
+++ installer/setup.sh
@@ -9,6 +9,8 @@
 ##  - bash <(wget -qO- https://raw.githubusercontent.com/irfnrdh/termux-ind/main/installer/setup.sh )	 ##
 ###########################################################################################################	
 
+set -eu -o pipefail 
+
 ## ANSI Colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
 MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
